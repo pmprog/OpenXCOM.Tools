b8792ba79edb167e00dc7e2ca2292364ffe9dbee
TheBigSot <sot_c2@hotmail.com>
2014-07-12 09:16:27 -0400

    Setup with VS 2012 Organized some forms Created the ErrorWindow Setup Error Handling in Application_ThreadException

---
+++ MapView/Forms/Error/ErrorWindowAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MapView.Forms.Error
+{
+    public class ErrorWindowAdapter : IErrorHandler
+    {
+        public void HandleException(Exception exception)
+        {
+            using (var window = new ErrorWindow(exception))
+            {
+                window.ShowDialog();
+            }
+        }
+    }
+}
